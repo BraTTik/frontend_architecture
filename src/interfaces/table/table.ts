f7c9390ef572471f28cd43eb0b8f1b824f2d01e3
brattik <bulki86@gmail.com>
2022-09-26 12:36:01 +0300

    удалена зависимость от типа в шапке таблицы

--- src/interfaces/table/table.ts
+++ src/interfaces/table/table.ts
@@ -1,15 +1,16 @@
 import React from "react";
 
+type ColumnId = string;
+
 type ColumnConfig = {
-    id: string;
     title: string;
     width: number;
 }
 
-type TableHeader<T extends object> = Record<keyof T, ColumnConfig>;
+type TableHeader = Record<ColumnId, ColumnConfig>;
 
 export interface Table <T extends object>{
     data: T[];
-    header: TableHeader<T>;
-    rowRenderer: (props: { element: T, index: number, data: T[], columnConfig: ColumnConfig }) => React.ReactElement;
+    header: TableHeader;
+    rowRenderer: (props: { item: T, index: number, data: T[], headerConfig: TableHeader }) => React.ReactElement;
 }
